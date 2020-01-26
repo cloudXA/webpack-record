2fbf6cc9fab060fb1efadfb6e8391b3164a1333f
cloudXA <zming794@gmail.com>
2020-01-26 18:46:41 +0800

    去重并分离代码

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import _ from 'lodash';
-import { cube } from './math.js';
-import './style.css';
 
 function component() {
     var element = document.createElement('pre');
 
     element.innerHTML = [
         'Hello webpack！hello world',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
+        '5 cubed is equal to ' 
+    ].join('\n\n\n');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
-
 
 module.exports = {
-  // 确保bundle是未压缩本
-  // entry: {
-  //   app: './src/index.js'
-  // },
-  entry: './src/index.js',
-  // output: {
-  //   filename: '[name].bundle.js',
-  //   path: path.resolve(__dirname, 'dist')
-  // },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  mode: 'productioin',
-  optimization: {
-    usedExports: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        another: './src/another-module.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    // 防止重复(prevent duplication)
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
         }
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader'
-          },
-        ],
-      },
+    },
+    devServer: {
+        contentBase: './dist',
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: "管理输出",
+        }),
+        new CleanWebpackPlugin()
     ]
-  },
-  devServer: {
-    contentBase: './dist',
-    open: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "模块热替换"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-};
-
-
-
+    
+}
